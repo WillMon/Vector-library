2b3d0fe9f7ba8b123e0ce393314dc4c8a7b84263
optildeath <w.montero34@gmail.com>
2016-01-08 14:08:24 -0600

    Updated Winter Assignment
    Prof telling me to get off my seat. Need to Update to move around about

--- Vector4.py
+++ Vector4.py
@@ -5,7 +5,7 @@ import math
 from math import *
 
 class Vector4(object):
-	def __init__(self, r=0,g=0,b=0,a=0, array = []):
+	def __init__(self, r=0,g=0,b=0,a=0):
 		self.r = r
 		self.g = g
 		self.b = b
